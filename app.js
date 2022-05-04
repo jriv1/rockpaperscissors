81db4386a4d94cb9fe51395f7cc8359109f5f8f2
Jorge Villanueva <jriv11011101@outlook.com>
2022-05-04 23:28:49 +0800

    Finished game logic

--- app.js
+++ app.js
@@ -1,22 +1,76 @@
 
 
+
 function computerPlay(){
 
     let computerChoice = Math.trunc(Math.random() * 3);
     if(computerChoice === 0){
-        console.log("Rock");
+       return("ROCK");
     }
     else if(computerChoice === 1){
-        console.log("Paper");
+        return("PAPER");
     }
     else if(computerChoice === 2){
-        console.log("Scissors")
+       return("SCISSORS")
     }
     else{
-        console.log("Error computer couldnt make a choice")
+       return ("Computer Error");
     }
    
 }
 
+function playerPlay(choice){
+    let choice1 = choice.toUpperCase();
+    return choice1;
+}
 
-computerPlay();
+
+
+let computerSelection;
+let playerSelection;
+
+
+    function play(computerSelection,playerSelection){
+        if(computerSelection === "ROCK" && playerSelection === "ROCK" || computerSelection === "PAPER"&& playerSelection ==="PAPER"|| computerSelection === "SCISSORS" && playerSelection === "SCISSORS"){
+            console.log("Draw");
+        }
+        else if(computerSelection === "ROCK" && playerSelection === "PAPER")
+        {
+            console.log("You Win!!!");
+        }
+        else if( computerSelection === "ROCK" && playerSelection === "SCISSORS"){
+            console.log("You Lose!!!")
+        }
+        else if(computerSelection === "PAPER"&& playerSelection === "SCISSORS")
+        {
+            console.log("You Win!!!");
+        }
+        else if( computerSelection === "PAPER"&& playerSelection === "ROCK"){
+            console.log("You lose!!!")
+        }
+        else if(computerSelection === "SCISSORS" && playerSelection === "ROCK")
+        {
+            console.log("You Win!!!");
+        }
+        else if( computerSelection === "SCISSORS" && playerSelection === "PAPER"){
+            console.log("You lose!!!")
+        }
+        else{
+            console.log("Err")
+        }
+    
+    }
+
+
+function game()
+{
+    for(let i = 0; i < 5; i++)
+    {
+        
+        playerSelection = playerPlay(prompt("Rock paper scissors?"));
+        computerSelection = computerPlay();
+        play(computerSelection,playerSelection);
+   } 
+       
+}
+game();
