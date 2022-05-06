cbc8ea0effaee088be1be80b3b9bb36bcaf7eda7
Jorge Villanueva <jriv11011101@outlook.com>
2022-05-06 16:21:44 +0800

    Game logic done except 5 rounds

--- app.js
+++ app.js
@@ -1,75 +1,90 @@
-
-
+const rock = document.querySelector(".btn-rock");
+const paper = document.querySelector(".btn-paper");
+const scissors = document.querySelector(".btn-scissors");
+const result = document.querySelector(".result");
+const playerChoice = document.querySelector(".playerChoice");
+const computerChoice = document.querySelector(".computerChoice");
+const playerScore = document.querySelector(".player-score");
+const computerScore = document.querySelector(".computer-score");
+let cScore = 0;
+let pScore = 0;
 
 function computerPlay(){
-
+    
     let computerChoice = Math.trunc(Math.random() * 3);
     if(computerChoice === 0){
-       return("ROCK");
+        return ("ROCK");
     }
     else if(computerChoice === 1){
-        return("PAPER");
+        return ("PAPER");
     }
     else if(computerChoice === 2){
-       return("SCISSORS")
+        return("SCISSORS");
     }
     else{
-       return ("Computer Error");
+        return("ERROR");
     }
-   
+    
 }
-
-function playerPlay(choice){
-    let choice1 = choice.toUpperCase();
-    return choice1;
-}
-
-
-
-let computerSelection;
-let playerSelection;
-
-
-    function play(computerSelection,playerSelection){
-        if(computerSelection === "ROCK" && playerSelection === "ROCK" || computerSelection === "PAPER"&& playerSelection ==="PAPER"|| computerSelection === "SCISSORS" && playerSelection === "SCISSORS"){
-            console.log("Draw");
+ 
+function play(computerSelection,playerSelection){
+      
+        playerChoice.textContent = playerSelection;
+        computerChoice.textContent = computerSelection;
+    
+        if(computerSelection === playerSelection){
+           result.textContent = "Draw";
         }
-        else if(computerSelection === "ROCK" && playerSelection === "PAPER")
-        {
-            console.log("You Win!!!");
+        else if(computerSelection === "ROCK" && playerSelection === "PAPER")       {    
+           result.textContent = "You win!!!";
+           pScore += 1;
+           
         }
-        else if( computerSelection === "ROCK" && playerSelection === "SCISSORS"){
-            console.log("You Lose!!!")
+        else if( computerSelection === "ROCK" && playerSelection === "SCISSORS"){     
+           result.textContent = "You lose!!!"
+           cScore += 1;
+          
         }
-        else if(computerSelection === "PAPER"&& playerSelection === "SCISSORS")
-        {
-            console.log("You Win!!!");
-        }
+        else if(computerSelection === "PAPER"&& playerSelection === "SCISSORS")    {          
+           result.textContent = "You win!!!";
+           pScore +=1;
+          
+        }     
         else if( computerSelection === "PAPER"&& playerSelection === "ROCK"){
-            console.log("You lose!!!")
+           
+           result.textContent = "You lose!!!"
+           cScore +=1;
         }
-        else if(computerSelection === "SCISSORS" && playerSelection === "ROCK")
-        {
-            console.log("You Win!!!");
+        else if(computerSelection === "SCISSORS" && playerSelection === "ROCK")      {
+           
+           result.textContent = "You win!!!";
+           pScore +=1;
         }
         else if( computerSelection === "SCISSORS" && playerSelection === "PAPER"){
-            console.log("You lose!!!")
+             
+           result.textContent = "You lose!!!";
+           cScore += 1;
         }
         else{
-            console.log("Err")
-        }
-    
+           result.textContent = "Err"
+        }   
+        console.log(pScore,cScore);
+        playerScore.textContent = pScore;
+        computerScore.textContent = cScore;
     }
 
 
-function game()
-{
-    for(let i = 0; i < 5; i++)
-    {
-        
-        playerSelection = playerPlay(prompt("Rock paper scissors?"));
-        computerSelection = computerPlay();
-        play(computerSelection,playerSelection);
-   } 
-       
-}
+
+    rock.addEventListener("click",function(){
+   play(computerPlay(),"ROCK");
+  });
+     
+    paper.addEventListener("click",function(){
+        play(computerPlay(),"PAPER");
+     });
+ 
+    scissors.addEventListener("click",function(){
+       play(computerPlay(),"SCISSORS");
+    });
+
+   
